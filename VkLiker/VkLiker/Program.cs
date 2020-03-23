df64a8f027eff3dd42bef14b7c020e0b7a4d28b5
holymosh <dpopov1997@gmail.com>
2020-03-24 01:53:32 +0300

    initial commit with solution

---
+++ VkLiker/VkLiker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VkLiker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

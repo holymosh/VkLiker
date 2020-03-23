2dc39bdd826345678fb0d345d82429ac9f82f936
holymosh <dpopov1997@gmail.com>
2020-03-24 02:21:22 +0300

    BaseEntity for domain model classes

---
+++ VkLiker/Domain/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain
+{
+    public abstract class BaseEntity
+    {
+        [ForeignKey("Id")]
+        public int Id { get; set; }
+
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
+    }
+}
